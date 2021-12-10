27cc95243aab7d815127726b003f85e5997d4159
gbemisoye <olalekanolalere1804@gmail.com>
2021-12-10 14:44:39 +0100

    code challenge wk2

---
+++ GroupAQuestion1/GroupAQuestion1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GroupAQuestion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+        public int findDigits(int number)
+        {
+            Console.Write("Enter a number :");
+            int numb = int.Parse(Console.ReadLine());
+            string n = numb.ToString();
+
+            int count = 0;
+            for (int d = n[0]; d < n.Length; d++)
+            {
+                if (numb % d == 0)
+                    count += d;
+            }
+            Console.WriteLine($"{count}");
+
+
+            return number;
+        }
+    }
+}
